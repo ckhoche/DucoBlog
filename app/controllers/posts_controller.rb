6b5ac7092b881b2b0c8662597ba794365066db33
ckhoche <ckhoche@gmail.com>
2017-05-23 22:58:15 -0700

    Add files via upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,51 @@
 class PostsController < ApplicationController
-  def new
+  http_basic_authenticate_with name: "duco1@test.com", password: "secret", except: [:index, :show]
+  
+  def index
+      @posts = Post.all
   end
- 
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(params.require(:post).permit(:title, :text))
    
-    @post.save
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+end
+
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(article_params)
     redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to articles_path
 end
 
 private
-  def article_params
+  def post_params
     params.require(:post).permit(:title, :text)   
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523014440) do
+ActiveRecord::Schema.define(version: 20170523215934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20170523014440) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "userId"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 20170523014440) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
